247a8878700e4970cbf3d6544fa3c06022780318
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-03 18:34:39 -0400

    pseudocode for exercise 24

---
+++ src/main/java/exercise24/solution24.java
@@ -0,0 +1,29 @@
+package exercise24;
+
+public class solution24 {
+
+    /*
+    Print "Enter two strings, and i'll tell you if they are anagrams:\n
+
+    print "Enter the first string: "
+    str1 = user input
+
+    print "Enter the second string: "
+    str2 user input
+
+    using if else loop determine if the string are or are not anagrams
+
+    using a boolean isAnagram
+
+    compare the length of the strings
+    if it isn't the same end the code there and return the exception
+
+    if it is the same length sort the array and see if the characters are the same.
+
+
+
+     */
+
+
+
+}
